27eaa2644b20fdc67e3df1e557966556cd6edd47
Foz3R <pawel.knot.1@gmail.com>
2023-06-01 18:12:08 +0200

    without logging

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { useAuth } from "@/context/auth";
+// import { useRouter } from "next/navigation";
+// import { useAuth } from "@/context/auth";
 
 import Header from "./components/header/header";
 import SidebarMenu from "./components/sidebar-menu/sidebar-menu";
@@ -11,16 +11,17 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const { currentUser } = useAuth();
-  const router = useRouter();
-  if (!currentUser) router.push("/signin");
+  // const { currentUser } = useAuth();
+  // const router = useRouter();
+  // if (!currentUser) router.push("/signin");
 
   return (
     <div className="overflow-hidden font-inter text-kolumbGray-900 ">
       <Header />
       <div className="flex bg-white">
         <SidebarMenu />
-        {currentUser && children}
+        {children}
+        {/* {currentUser && children} */}
       </div>
     </div>
   );
