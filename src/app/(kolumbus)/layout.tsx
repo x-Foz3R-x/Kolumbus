8f0244333caea8e884cdb4d63015a6fb44b9e96f
Foz3R <pawel.knot.1@gmail.com>
2023-10-12 13:24:22 +0200

    updated middleware

--- src/app/(kolumbus)/layout.tsx
+++ src/app/(kolumbus)/layout.tsx
@@ -1,9 +1,33 @@
 import { AppdataProvider } from "@/context/appdata";
+import { currentUser } from "@clerk/nextjs";
 
 import Header from "@/components/layouts/header";
 import UnsupportedWidth from "@/components/layouts/unsupportedWidth";
+import { prisma } from "@/lib/prisma";
+import { GenerateItinerary } from "@/lib/utils";
+import { Trip } from "@/types";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  // const user = await currentUser();
+
+  // const trips = await prisma.trip.findMany({
+  //   where: { userId: user?.id },
+  //   orderBy: { position: "asc" },
+  // });
+
+  // for (let i = 0; i < trips.length; i++) {
+  //   const trip = trips[i];
+
+  //   const events = await prisma.event.findMany({
+  //     where: { tripId: trip.id },
+  //     orderBy: [{ position: "asc" }],
+  //   });
+
+  //   (trip as Trip).itinerary = GenerateItinerary(trip.id, trip.startDate, trip.endDate, events);
+  // }
+
+  // console.log(trips);
+
   return (
     <AppdataProvider>
       {/* <div className="supported-width bg-white font-inter"> */}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/google(.*)"],
+  publicRoutes: ["/", "/api/google(.*)", "/sign-in", "/sign-up"],
 });
 
 export const config = {
