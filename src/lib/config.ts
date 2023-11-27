3f6c917c2f244dad070778988c8f5659879bbd51
Foz3R <pawel.knot.1@gmail.com>
2023-11-27 17:15:54 +0100

    Update package.json and config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,2 @@
 export const BASE_URL = process.env.NODE_ENV === "production" ? "https://www.kolumbus.app" : "http://localhost:3000";
-export const LANGUAGE = !(typeof window !== "undefined" && window.document && window.document.createElement)
-  ? navigator.language.split("-")[0]
-  : "en";
+export const LANGUAGE = navigator && navigator.language.split("-")[0];
