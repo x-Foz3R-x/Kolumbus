ec2f54cf8853ff6279a1c2d02be8998414f1f2e7
Foz3R <pawel.knot.1@gmail.com>
2023-11-27 17:12:12 +0100

    Update LANGUAGE constant in config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,4 @@
 export const BASE_URL = process.env.NODE_ENV === "production" ? "https://www.kolumbus.app" : "http://localhost:3000";
-export const LANGUAGE = navigator.language.split("-")[0];
+export const LANGUAGE = !(typeof window !== "undefined" && window.document && window.document.createElement)
+  ? navigator.language.split("-")[0]
+  : "en";
