8d89d40810d675c4eb242608ce295d5eca65a365
Foz3R <pawel.knot.1@gmail.com>
2023-11-27 17:30:25 +0100

    Update version number in package.json

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,3 @@
-"use client";
-
 export const BASE_URL = process.env.NODE_ENV === "production" ? "https://www.kolumbus.app" : "http://localhost:3000";
 
 // There are some issues with this navigator.
