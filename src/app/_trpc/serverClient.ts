44e721a65518ccbd3422691210fda29a8fff3c47
Foz3R <pawel.knot.1@gmail.com>
2023-11-27 15:22:45 +0100

    Update API URLs to use HTTPS in production

--- src/app/_trpc/serverClient.ts
+++ src/app/_trpc/serverClient.ts
@@ -1,7 +1,7 @@
 import { httpBatchLink } from "@trpc/client";
 import { appRouter } from "@/server";
 
-const url = process.env.NODE_ENV === "production" ? "kolumbus.app/api/trpc" : "http://localhost:3000/api/trpc";
+const url = process.env.NODE_ENV === "production" ? "https://kolumbus.app/api/trpc" : "http://localhost:3000/api/trpc";
 
 export const serverApi = appRouter.createCaller({
   links: [httpBatchLink({ url })],
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/(.*)", "/api/google(.*)", "/sign-in", "/sign-up"],
-  apiRoutes: ["/api(.*)"],
+  publicRoutes: ["/", "/playground(.*)", "/sign-in", "/sign-up"],
+  apiRoutes: ["/(api|trpc)(.*)"],
 });
 
 export const config = {
