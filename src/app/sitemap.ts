17ea2712c2fb42187d79ecf46851db4581905165
Foz3R <pawel.knot.1@gmail.com>
2023-12-05 15:32:51 +0100

    refactor: sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,87 +1,90 @@
 /**
  * @category Sitemap
- * @version 1.0.0
+ * @updated 2023-12-05
  *
- * @description
- * This file contains the sitemap of the application.
- * Priority schema:
- * - 1: Home page.
- * - 0.9: Auth.
- * - 0.8: Important and frequently accessed pages (default).
- * - 0.5: Moderately important pages.
- * - 0.1: Easter egg pages.
- * - 0: Utility pages.
+ * Priority grade system:
+ * - 1.0: Critical - Reserved for home page or critical landing pages.
+ * - 0.9: Very High - Assigned to pages of very high importance, such as key conversion funnels or crucial user interaction points.
+ * - 0.8: High - Given to important and frequently accessed pages, like sign-in, sign-up, or major sections of your website.
+ * - 0.7: Significant - Used for pages that are significant but slightly less critical in importance.
+ * - 0.6: Above Average - Assigned to above-average important pages.
+ * - 0.5: Moderate - Applied to moderately important pages, like legal pages or privacy policies.
+ * - 0.4: Below Average - Given to pages of below-average importance.
+ * - 0.3: Low - Reserved for pages of low importance in the overall structure of the site.
+ * - 0.2: Very Low - Assigned to pages of very low importance or pages that are more like Easter eggs.
+ * - 0.1: Minimal - Used for utility pages or pages with minimal importance in the context of the application.
+ * - 0.0: Excluded - Indicates that the page should be excluded from the sitemap or considered the lowest possible priority.
  */
 export default async function sitemap() {
   return [
     {
       url: "https://www.kolumbus.app/",
-      lastModified: "06-12-2023",
-      priority: 1,
+      lastModified: "2023-12-05",
+      priority: 1.0,
     },
     {
       url: "https://www.kolumbus.app/sign-in",
-      lastModified: "06-12-2023",
-      priority: 0.9,
-    },
-    {
-      url: "https://www.kolumbus.app/sign-up",
-      lastModified: "06-12-2023",
-      priority: 0.9,
-    },
-    {
-      url: "https://www.kolumbus.app/contact",
-      lastModified: "06-12-2023",
+      lastModified: "2023-12-05",
       priority: 0.8,
     },
     {
+      url: "https://www.kolumbus.app/sign-up",
+      lastModified: "2023-12-05",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.kolumbus.app/contact",
+      lastModified: "2023-12-05",
+      priority: 0.6,
+    },
+    {
       url: "https://www.kolumbus.app/sitemap",
-      lastModified: "06-12-2023",
-      priority: 0.5,
+      lastModified: "2023-12-05",
+      priority: 0.4,
     },
     //#region Legal
     {
       url: "https://www.kolumbus.app/legal",
-      lastModified: "06-12-2023",
-      priority: 0,
+      lastModified: "2023-12-05",
+      priority: 0.4,
     },
     {
       url: "https://www.kolumbus.app/legal/privacy",
-      lastModified: "06-12-2023",
+      lastModified: "2023-12-05",
       priority: 0.5,
     },
     {
       url: "https://www.kolumbus.app/legal/terms",
-      lastModified: "06-12-2023",
+      lastModified: "2023-12-05",
       priority: 0.5,
     },
     //#endregion
     //#region Playground
     {
       url: "https://www.kolumbus.app/playground",
-      lastModified: "06-12-2023",
-      priority: 0.1,
+      lastModified: "2023-12-05",
+      priority: 0.2,
     },
     {
       url: "https://www.kolumbus.app/playground/button",
-      lastModified: "06-12-2023",
-      priority: 0.1,
+      lastModified: "2023-12-05",
+      priority: 0.2,
     },
     {
       url: "https://www.kolumbus.app/playground/color",
-      lastModified: "06-12-2023",
-      priority: 0.1,
+      lastModified: "2023-12-05",
+      priority: 0.2,
     },
     {
       url: "https://www.kolumbus.app/playground/dropdown",
-      lastModified: "06-12-2023",
-      priority: 0.1,
+      lastModified: "2023-12-05",
+      priority: 0.2,
     },
     //#endregion
     {
       url: "https://www.kolumbus.app/t/guest",
-      lastModified: "06-12-2023",
-      priority: 0.8,
+      lastModified: "2023-12-05",
+      priority: 0.7,
     },
   ];
 }
