7fb26254f5626e9e94fe57fc7cad922e97db54bc
Foz3R <pawel.knot.1@gmail.com>
2023-12-14 19:07:49 +0100

    layout.tsx; feat: add SpeedInsights and BetaWatermark components refactor: update font imports, metadata, and components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,37 @@
-import { Analytics } from "@vercel/analytics/react";
-import { inter, gordita, adso, inconsolata } from "../assets/fonts/fonts";
 import { ClerkProvider } from "@clerk/nextjs";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+
+import { inter, gordita, adso, inconsolata } from "./fonts";
 import TrpcProvider from "./_trpc/Provider";
+import { Metadata } from "next";
 import "./tailwind.css";
 
-export const metadata = {
-  title: {
-    default: "Kolumbus",
-  },
+import BetaWatermark from "@/components/beta-watermark";
+
+export const metadata: Metadata = {
+  title: "Kolumbus",
+  applicationName: "Kolumbus",
+  category: "Trip planner",
+  creator: "Foz3R",
   description:
-    "Discover the ultimate travel experience with our Trip Planning App. Craft personalized itineraries effortlessly. Seamlessly plan your adventures and explore the world like never before.",
+    "Turn dreams into reality with Kolumbus! 🌟 From dream to itinerary, plan your trips effortlessly and savor the freedom to explore. Your journey, your way - Kolumbus, where adventure begins!",
 };
 
-type RootLayoutProps = { children: React.ReactNode };
-
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
-      {/* <html lang="en" style={{ fontSize: "16px" }} className="dark"> */}
       <html lang="en" style={{ fontSize: "16px" }}>
         <body
-          className={`h-screen w-screen scroll-smooth fill-gray-900 text-gray-900 antialiased selection:bg-kolumblue-300 ${gordita.className} ${gordita.variable} ${adso.variable} ${inter.variable} ${inconsolata.variable}
+          className={`h-screen w-screen scroll-smooth fill-gray-900 text-gray-900 antialiased ${gordita.className} ${gordita.variable} ${adso.variable} ${inter.variable} ${inconsolata.variable}
           `}
         >
-          <TrpcProvider>{children}</TrpcProvider>
+          <TrpcProvider>
+            {children}
+            <BetaWatermark />
+          </TrpcProvider>
+
+          <SpeedInsights debug={false} />
           <Analytics debug={false} />
         </body>
       </html>
