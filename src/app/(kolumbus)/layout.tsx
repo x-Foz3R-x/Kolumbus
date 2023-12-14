586595d5aa0baaa07448a45a1803f11ae8469035
Foz3R <pawel.knot.1@gmail.com>
2023-12-14 19:08:55 +0100

    refactor: update (kolumbus)/layout.tsx to use absolute import paths and convert trip dates to ISO string

--- src/app/(kolumbus)/layout.tsx
+++ src/app/(kolumbus)/layout.tsx
@@ -1,32 +1,10 @@
-import { serverApi } from "../_trpc/serverClient";
+import { serverApi } from "@/app/_trpc/serverClient";
 import { AppdataProvider } from "@/context/appdata";
-
-import type { ServerTrip } from "@/server/routers/trip";
-import type { Trip } from "@/types";
-
-import Header from "@/components/layouts/header";
-import UnsupportedWidth from "@/components/layouts/unsupportedWidth";
+import { ConvertTripDatesToISOString } from "@/lib/utils";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
-  let trips: (Trip | ServerTrip)[] = (await serverApi.trip.getAllWithEvents()) ?? [];
+  // if no user get trips data from local storage
+  const trips = ((await serverApi.trip.getAllWithEvents()) ?? []).map(ConvertTripDatesToISOString);
 
-  trips = trips?.map((trip) => {
-    if (trip.startDate instanceof Date) trip.startDate = trip.startDate.toISOString();
-    if (trip.endDate instanceof Date) trip.endDate = trip.endDate.toISOString();
-    if (trip.updatedAt instanceof Date) trip.updatedAt = trip.updatedAt.toISOString();
-    if (trip.createdAt instanceof Date) trip.createdAt = trip.createdAt.toISOString();
-
-    return trip as Trip;
-  });
-
-  return (
-    <AppdataProvider trips={trips as Trip[]}>
-      {/* <div className="supported-width bg-white font-inter"> */}
-      <div className="bg-white font-inter">
-        <Header />
-        {children}
-      </div>
-      {/* <UnsupportedWidth /> */}
-    </AppdataProvider>
-  );
+  return <AppdataProvider trips={trips}>{children}</AppdataProvider>;
 }
