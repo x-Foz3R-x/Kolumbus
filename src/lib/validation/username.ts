648e9b2c2fade5bffbae2c10f4ea039fd24d973c
Foz3R <pawel.knot.1@gmail.com>
2023-12-18 19:28:48 +0100

    Refactor name validation and add name validation

--- src/lib/validation/username.ts
+++
@@ -1,9 +0,0 @@
-import isBlacklisted from "./username-blacklist";
-
-export function ValidateUsername(username: string) {
-  return {
-    length: username.length >= 5 && username.length <= 32,
-    characters: /^[-_]{0,3}[a-zA-Z0-9]+([-_][a-zA-Z0-9]+)*[-_]{0,3}$/.test(username),
-    allowed: !isBlacklisted(username.toLowerCase()),
-  };
-}
