1558b37811738134e8d14bee6922a930d2414de0
Foz3R <pawel.knot.1@gmail.com>
2024-01-17 03:15:23 +0100

    Refactor skeleton placeholders for action-bar and itinerary

--- src/components/loading/action-bar-skeleton.tsx
+++ src/components/loading/action-bar-skeleton.tsx
@@ -1,16 +1,18 @@
+import Skeleton from "../ui/skeleton";
+
 export function ActionBarSkeleton() {
   return (
-    <div className="z-30 flex w-full p-3">
+    <section className="sticky left-0 right-0 top-14 z-30 flex w-full min-w-min pb-3 pl-3 pr-6 font-inter">
       <div className="flex h-14 w-full items-center justify-between gap-5 rounded-lg border border-gray-100 bg-white/80 shadow-xl backdrop-blur-[20px] backdrop-saturate-[180%] backdrop-filter">
-        <section className="flex h-full w-full items-center gap-2 overflow-x-scroll pl-3">
-          <div className="h-8 w-32 rounded bg-black/5 px-2 py-1" />
-        </section>
+        <div className="flex h-full w-full items-center overflow-x-auto pl-3">
+          <Skeleton className="h-8 w-32 rounded bg-black/5" />
+        </div>
 
-        <section className="flex flex-shrink-0 items-center gap-2 pr-5">
-          <div className="h-9 w-9 rounded bg-black/5" />
-          <div className="h-9 w-20 rounded bg-black/5" />
-        </section>
+        <div className="flex flex-shrink-0 items-center gap-2 pr-5">
+          <Skeleton className="h-[38px] w-[35px] rounded bg-black/5" />
+          <Skeleton className="h-[38px] w-[81.5px] rounded bg-black/5" />
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
--- src/components/loading/itinerary-skeleton.tsx
+++ src/components/loading/itinerary-skeleton.tsx
@@ -1,25 +1,34 @@
 import Skeleton from "../ui/skeleton";
 
+// todo - add events to skeleton (3,0,2,4,1 -> number of events per day)
+
 export function ItinerarySkeleton() {
-  const days = Array.from({ length: 5 }, (_, i) => i);
+  const days = [3, 0, 2, 4, 1];
 
   return (
-    <div className="flex h-full w-full gap-5 px-6">
-      <ul className="overflow-hidden rounded-xl">
-        {days.map((day) => (
-          <span key={day}>
-            <Skeleton className="h-5 w-32 bg-kolumblue-200" />
-            <Skeleton className="h-28 w-32 bg-gray-50" />
-          </span>
-        ))}
-        <Skeleton className="relative z-30 block h-5 w-32 bg-kolumblue-200" />
-      </ul>
+    <ul className="flex w-full min-w-fit flex-col px-6">
+      {days.map((dayEvents, index) => (
+        <li key={index} className="group/day flex w-full gap-5">
+          <div>
+            {/* Calendar Header */}
+            <Skeleton className="h-5 w-32 bg-kolumblue-200 group-first/day:rounded-t-xl" />
 
-      <ul className="flex flex-col gap-5 py-5">
-        {days.map((day) => (
-          <Skeleton key={day} className="h-28 w-8 rounded-xl bg-gray-50" />
-        ))}
-      </ul>
-    </div>
+            {/* Calendar */}
+            <Skeleton className="h-28 w-32 bg-gray-50" />
+          </div>
+
+          {/* Events list */}
+          <ul className="mt-5 flex h-28 w-full list-none gap-2">
+            {Array.from({ length: dayEvents }).map((_, index) => (
+              <Skeleton key={index} className="h-28 w-40 rounded-lg bg-black/5" />
+            ))}
+            <Skeleton className="h-28 w-8 rounded-xl bg-black/5" />
+          </ul>
+        </li>
+      ))}
+
+      {/* Calendar End */}
+      <Skeleton className="h-5 w-32 rounded-b-xl bg-kolumblue-200" />
+    </ul>
   );
 }
