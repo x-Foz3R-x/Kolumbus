3a02a768ea2eb44a26abb1837698ea94b8221000
Foz3R <pawel.knot.1@gmail.com>
2024-04-16 12:59:57 +0200

    Add uploadthing hook helper to create custom upload components

--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,4 +1,5 @@
 import {
+  generateReactHelpers,
   generateUploadButton,
   generateUploadDropzone,
 } from "@uploadthing/react";
@@ -7,3 +8,5 @@ import type { KolumbusFileRouter } from "~/app/api/uploadthing/core";
 
 export const UploadButton = generateUploadButton<KolumbusFileRouter>();
 export const UploadDropzone = generateUploadDropzone<KolumbusFileRouter>();
+
+export const { useUploadThing } = generateReactHelpers<KolumbusFileRouter>();
