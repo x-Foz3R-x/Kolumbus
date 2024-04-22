6b05719fd50f46dd9defcddd61a1a2e8046d631c
Foz3R <pawel.knot.1@gmail.com>
2024-04-22 17:38:51 +0200

    Update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ import plugin from "tailwindcss/plugin";
 import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
 
 export default {
-  content: ["./src/**/*.{js,ts,tsx}"],
+  content: ["./src/**/*.{ts,tsx}"],
   darkMode: "selector",
   theme: {
     extend: {
@@ -26,6 +26,19 @@ export default {
           800: "hsl(210, 78%, 27%)",
           900: "hsl(210, 78%, 17%)",
           950: "hsl(210, 78%, 11%)",
+        },
+        gray: {
+          50: "#fafafa",
+          100: "#f4f4f5",
+          200: "#e4e4e7",
+          300: "#d4d4d8",
+          400: "#a1a1aa",
+          500: "#71717a",
+          600: "#52525b",
+          700: "#3f3f46",
+          800: "#27272a",
+          900: "#18181b",
+          950: "#09090b",
         },
       },
       animation: {
@@ -85,8 +98,6 @@ export default {
         md: "0 0 5px rgba(0,0,0,0.05), 0 2px 2px rgba(0,0,0,0.02), 0 4px 4px rgba(0,0,0,0.02), 0 8px 8px rgba(0,0,0,0.02), 0 12px 12px rgba(0,0,0,0.01)",
         lg: "0 0 5px rgba(0,0,0,0.05), 0 2px 2px rgba(0,0,0,0.02), 0 4px 4px rgba(0,0,0,0.02), 0 8px 8px rgba(0,0,0,0.02), 0 12px 12px rgba(0,0,0,0.01)",
         xl: "0 0 5px rgba(0,0,0,0.02), 0 2px 2px rgba(0,0,0,0.02), 0 4px 4px rgba(0,0,0,0.02), 0 8px 8px rgba(0,0,0,0.02), 0 16px 16px rgba(0,0,0,0.02)",
-        "2xl":
-          "0 0 5px rgba(0,0,0,0.05), 0 2px 2px rgba(0,0,0,0.03), 0 4px 4px rgba(0,0,0,0.03), 0 8px 8px rgba(0,0,0,0.03), 0 12px 12px rgba(0,0,0,0.03), 0 16px 16px rgba(0,0,0,0.03)",
 
         borderXS:
           "0 0 0 1px rgba(0,0,0,0.1), 0 1px 2px rgba(0,0,0,0.02), 0 2px 2px rgba(0,0,0,0.01), 0 2px 3px rgba(0,0,0,0.01)",
