637349f5c1050956029e071402baef0bc8d02ac1
Foz3R <pawel.knot.1@gmail.com>
2024-04-24 17:45:13 +0200

    Add stage manager

---
+++ src/components/stage-manager.tsx
@@ -0,0 +1,63 @@
+import { motion } from "framer-motion";
+import { EASING } from "~/lib/motion";
+import { cn } from "~/lib/utils";
+
+type InitialStage = { className?: string; children: React.ReactNode };
+type Stage = { prevStageHeight: number; className?: string; children: React.ReactNode };
+
+export default function StageManager(props: {
+  stage: number;
+  stages: [InitialStage, ...Stage[]];
+  gap?: number;
+  style?: React.CSSProperties;
+  className?: string;
+}) {
+  return (
+    <div style={props.style} className={cn("w-full overflow-y-clip", props.className)}>
+      {props.stages.map((stage, index) => {
+        const isActive = props.stage === index;
+        const movementDirection = props.stage < index ? "up" : "down";
+
+        const rotate = isActive ? "0deg" : `${movementDirection === "up" ? "-" : ""}90deg`;
+        const translateY =
+          isActive || movementDirection === "down"
+            ? "0px"
+            : "prevStageHeight" in stage && stage.prevStageHeight
+              ? `${stage.prevStageHeight + (props?.gap ?? 16)}px`
+              : "0px";
+        const classNames = cn(
+          movementDirection === "up" ? "origin-bottom" : "origin-top",
+          stage.className,
+        );
+
+        return (
+          <motion.div
+            key={index}
+            initial={{
+              opacity: isActive ? 1 : 0,
+              scaleX: isActive ? 1 : 0.85,
+              height: isActive ? "auto" : "0",
+              rotateX: isActive ? "0deg" : rotate,
+              y: translateY,
+            }}
+            animate={{
+              opacity: isActive ? 1 : 0,
+              scaleX: isActive ? 1 : 0.85,
+              height: isActive ? "auto" : "0",
+              rotateX: isActive ? "0deg" : rotate,
+              y: translateY,
+            }}
+            transition={{
+              ease: EASING.easeInOut,
+              duration: 0.7,
+              opacity: { ease: EASING.easeInOut, duration: 0.4, delay: isActive ? 0 : 0.3 },
+            }}
+            className={classNames}
+          >
+            {stage.children}
+          </motion.div>
+        );
+      })}
+    </div>
+  );
+}
