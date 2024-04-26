b26db1cf0a224de14ce979dac9358ac3cdad9b11
Foz3R <pawel.knot.1@gmail.com>
2024-04-26 18:14:43 +0200

    Update title in library layout

--- src/app/library/layout.tsx
+++ src/app/library/layout.tsx
@@ -2,7 +2,7 @@ import TopNav from "./_components/top-nav";
 import GlobalFooter from "~/components/layouts/global-footer";
 
 export const metadata = {
-  title: "Library - Kolumbus",
+  title: "Library",
   description:
     "Browse your personal and shared trips, create new adventures, duplicate existing trips for easy planning, or delete trips as needed. It's your one-stop destination for trip management.",
 };
