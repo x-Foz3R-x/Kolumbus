8095e948cf18b8840e2096e7c2819b10f5e76372
Foz3R <pawel.knot.1@gmail.com>
2024-05-21 15:10:54 +0200

    Adjust sentry configs

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -8,7 +8,7 @@ Sentry.init({
   dsn: "https://7a8713b9f41e8c19576d76e5d660845d@o4507095547248640.ingest.de.sentry.io/4507095565598800",
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
