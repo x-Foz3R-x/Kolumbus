ac2da719195d47dfba2a3b03cc7c1326de3f973c
Foz3R <pawel.knot.1@gmail.com>
2024-05-25 12:38:47 +0200

    refactor: Remove unused code and update date picker in top-nav.tsx

--- src/app/t/[tripId]/_components/top-nav.tsx
+++ src/app/t/[tripId]/_components/top-nav.tsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { format } from "date-fns";
 
 import { api } from "~/trpc/react";
 import { useTripContext } from "./trip-provider";
@@ -60,56 +59,12 @@ export default function TopNav() {
 
       <section className="flex h-full flex-shrink-0 items-center justify-end gap-2">
         <div id="trash-container" />
-        <Icons.rangeCalendarOg className="h-[38px] fill-kolumblue-500" />
         <DatePicker
           startDate={trip.startDate}
           endDate={trip.endDate}
           maxDays={14}
           onApply={applyDateRange}
           includeDays
-        />
-        <DatePicker
-          startDate={trip.startDate}
-          endDate={trip.endDate}
-          maxDays={14}
-          onApply={(startDate, endDate) => {
-            console.log(startDate, endDate);
-          }}
-          buttonProps={{
-            variant: "unset",
-            size: "unset",
-            className: "relative h-10 fill-kolumblue-500",
-            tooltip: {
-              placement: "bottom",
-              offset: 8,
-              arrow: true,
-              focus: { enabled: false },
-              zIndex: 50,
-              children: "Date Picker",
-            },
-            children: (
-              <>
-                <Icons.rangeCalendar4 className="h-full" />
-                <div className="absolute inset-y-0 left-0 flex w-[40px] flex-col items-center justify-between pb-1 pl-1 pr-[7px] pt-[7px] leading-none">
-                  <span className="text-[10px] font-medium uppercase leading-[14px] tracking-tight text-white">
-                    {format(new Date(trip.startDate), "MMM").toUpperCase()}
-                  </span>
-                  <span className="text-sm leading-[15px]">
-                    {format(new Date(trip.startDate), "d")}
-                  </span>
-                </div>
-
-                <div className="absolute inset-y-0 right-0 flex w-[40px] flex-col items-center justify-between pb-1 pl-[7px] pr-1 pt-[7px] leading-none">
-                  <span className="text-[10px] font-medium uppercase leading-[14px] tracking-tight text-white">
-                    {format(new Date(trip.endDate), "MMM").toUpperCase()}
-                  </span>
-                  <span className="text-sm leading-[15px]">
-                    {format(new Date(trip.endDate), "d")}
-                  </span>
-                </div>
-              </>
-            ),
-          }}
         />
         <MembersDropdown
           tripId={trip.id}
