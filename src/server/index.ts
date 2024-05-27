5b46c61f1a65bf921975b7d513c2d8285b252a09
Foz3R <pawel.knot.1@gmail.com>
2024-05-27 22:23:22 +0200

    chore: Remove clerk router from server index and delete clerk.ts router file

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,14 +2,12 @@ import { router } from "./trpc";
 import trip from "./routers/trip";
 import event from "./routers/event";
 import google from "./routers/google";
-import clerk from "./routers/clerk";
 
 // Main router where you can add new routers.
 export const appRouter = router({
   trip,
   event,
   google,
-  clerk,
 });
 
 export type AppRouter = typeof appRouter;
