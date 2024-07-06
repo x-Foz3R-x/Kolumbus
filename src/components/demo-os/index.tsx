fb513ffe226c4c097988dca8b93a51b00fda9d1d
Foz3R <pawel.knot.1@gmail.com>
2024-07-06 18:12:20 +0200

    remove menu bar and update styles

--- src/components/demo-os/index.tsx
+++ src/components/demo-os/index.tsx
@@ -1,10 +1,15 @@
-import MenuBar from "./menu-bar";
 import Desktop from "./desktop";
 
 export default function DemoOs() {
   return (
-    <div className="shadow-xxl relative flex h-fit w-full max-w-screen-xl flex-col overflow-hidden rounded-3xl border-gray-700 bg-kolumblue-500/25 font-inter lg:w-[90%]">
-      <MenuBar />
+    <div className="relative flex h-fit w-full flex-col rounded-b-[3rem] rounded-tr-[3rem] bg-gray-800 py-24 font-inter">
+      <div className="text-scale-sm absolute -top-36 left-0 flex h-36 w-[40%] items-end justify-center rounded-t-[3rem] bg-gray-800 text-center font-belanosima font-bold text-white">
+        <p className="h-8 px-8">
+          Discover the <span className="text-yellow-300"> Power </span> of Kolumbus
+        </p>
+      </div>
+      <span className="absolute -top-36 left-[40%] h-36 w-12 bg-gray-800" />
+      <span className="absolute -top-36 left-[40%] h-36 w-12 rounded-bl-[3rem] bg-white" />
       <Desktop />
     </div>
   );
