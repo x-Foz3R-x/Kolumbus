1179a0b8033f5801f7f002868894e89eaaa74e5a
Foz3R <pawel.knot.1@gmail.com>
2024-07-07 10:14:57 +0200

    refactor: Update DemoOs component absolute elements arrangement

--- src/components/demo-os/index.tsx
+++ src/components/demo-os/index.tsx
@@ -3,13 +3,16 @@ import Desktop from "./desktop";
 export default function DemoOs() {
   return (
     <div className="relative flex h-fit w-full flex-col rounded-b-[3rem] rounded-tr-[3rem] bg-gray-800 py-24 font-inter">
-      <div className="text-scale-sm absolute -top-28 left-0 flex h-28 w-[40%] items-end justify-center rounded-t-[3rem] bg-gray-800 text-center font-belanosima font-bold text-white">
-        <p className="h-8 px-8">
-          Discover the <span className="text-yellow-300"> Power </span> of Kolumbus
+      <div className="absolute -top-28 left-0 flex h-56 w-[50%] items-center justify-center rounded-t-[3rem] bg-gray-800">
+        <p className="text-scale-sm text-balance px-[10%] text-center font-belanosima font-bold leading-none text-white">
+          Discover the <span className="text-yellow-300"> Power </span>
+          of Kolumbus
         </p>
+
+        <span className="absolute -right-12 bottom-1/2 size-12 bg-gray-800" />
+        <span className="absolute -right-12 bottom-1/2 size-12 rounded-bl-full bg-white" />
       </div>
-      <span className="absolute -top-28 left-[40%] h-28 w-12 bg-gray-800" />
-      <span className="absolute -top-28 left-[40%] h-28 w-12 rounded-bl-[3rem] bg-white" />
+
       <Desktop />
     </div>
   );
