cbd3fca13bc6aa0a07ed28e6bd377a7658f58d5b
Foz3R <pawel.knot.1@gmail.com>
2024-07-07 10:15:11 +0200

    Update Hero component styles and font weight

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -10,8 +10,8 @@ export default function Hero() {
         style={{ gap: "min(max(2.15rem, 2.5vw), 3.1rem)" }}
         className="relative flex flex-shrink-0 flex-col items-center justify-center text-center md:items-start md:text-left"
       >
-        <div className="whitespace-nowrap font-semibold">
-          <h1 className="text-scale-xl leading-tight text-kolumblue-500">Travel Planning</h1>
+        <div className="whitespace-nowrap font-bold">
+          <h1 className="text-scale-xl leading-none text-kolumblue-500">Travel Planning</h1>
           <h2 className="text-scale-sm">
             Made
             <span className="text-scale-lg text-green-500"> Simple </span>
@@ -24,7 +24,7 @@ export default function Hero() {
 
         <Link
           href="/signup"
-          className="group relative flex w-fit items-center rounded-xl border-2 border-black/10 bg-kolumblue-500 px-5 py-2 text-lg font-semibold text-white shadow-lg outline-kolumblue-200 duration-400 ease-kolumb-flow hover:rounded focus:shadow-focus lg:px-8 lg:py-3 lg:text-2xl"
+          className="group relative flex w-fit items-center rounded-xl border-2 border-black/10 bg-kolumblue-500 px-5 py-2 text-lg font-bold text-white shadow-lg outline-kolumblue-200 duration-400 ease-kolumb-flow hover:rounded focus:shadow-focus lg:px-8 lg:py-3 lg:text-2xl"
         >
           <Icons.arrowRight className="absolute left-px w-4 fill-white opacity-0 duration-400 ease-kolumb-flow group-hover:left-2 group-hover:opacity-100 lg:left-1 lg:w-5 lg:group-hover:left-4" />
 
