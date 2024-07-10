bec04c02f3f7a610b53cf2f954ed7eec4e25ad45
Foz3R <pawel.knot.1@gmail.com>
2024-07-10 15:45:12 +0200

    add custom logo component

---
+++ src/app/_components/logo.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+import { EASING } from "~/lib/motion";
+
+import { Icons } from "~/components/ui";
+
+export default function Logo() {
+  const [isExpanded, setExpanded] = useState(true);
+  const [isHovered, setHovered] = useState(false);
+
+  // Toggle animation state based on scroll position and direction
+  useEffect(() => {
+    const handleScroll = () => {
+      const isBeyondThreshold = window.scrollY > 100;
+      setExpanded(!isBeyondThreshold);
+    };
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <Link
+      href="/"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      onFocus={() => setHovered(true)}
+      onBlur={() => setHovered(false)}
+      className="absolute left-8 top-1.5 z-10 flex flex-shrink-0 items-center rounded-3xl border-2 border-double border-kolumblue-500 fill-kolumblue-500 p-0.5 text-2xl font-bold text-kolumblue-500 backdrop-blur-lg backdrop-saturate-[180%] duration-500 ease-kolumb-flow hover:rounded focus:rounded"
+    >
+      <Icons.logo className="h-[44px] flex-shrink-0" />
+      <motion.div
+        initial={{ width: "148px" }}
+        animate={{ width: isExpanded || isHovered ? "148px" : "0px" }}
+        transition={{ ease: EASING.kolumbFlow, duration: 0.5 }}
+        className="mt-1 overflow-hidden"
+      >
+        <span className="px-2">KOLUMBUS</span>
+      </motion.div>
+    </Link>
+  );
+}
--- src/app/_components/top-nav.tsx
+++ src/app/_components/top-nav.tsx
@@ -1,41 +1,40 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import NextLink from "next/link";
 
-import { Link, Icons, Divider } from "~/components/ui";
-import ProfileButton from "~/components/profile-button";
-import SlideAnimation from "./slide-animation";
-import { Menu, MenuLink, MenuOption } from "~/components/ui/menu";
 import { accountUrl } from "~/lib/constants";
+
+import Logo from "./logo";
+import SlideAnimation from "./slide-animation";
+import ProfileButton from "~/components/profile-button";
+import { Menu, MenuLink, MenuOption } from "~/components/ui/menu";
+import { Link, Icons, Divider } from "~/components/ui";
 
 export default function TopNav() {
   return (
     <nav className="apply-custom-cursor fixed inset-x-0 top-0 z-50 flex h-16 items-center px-8 font-belanosima">
-      <NextLink href="/" className="fill-kolumblue-500">
-        <Icons.logo className="m-auto h-12 rounded-full bg-white p-px outline-double outline-2 outline-offset-1 outline-kolumblue-500" />
-      </NextLink>
+      <Logo />
 
       <SlideAnimation
         direction="out"
         threshold={100}
-        className="relative flex h-16 w-full items-center justify-between"
+        className="relative flex h-16 w-full items-center justify-between gap-6 pl-[224px]"
       >
-        <div className="flex gap-4 text-gray-600">
-          <NextLink href="/" className="mt-1 pl-2 text-2xl font-bold">
-            KOLUMBUS
-          </NextLink>
+        <Link.Arrow href="/contact" theme="outline" size="md">
+          Contact
+        </Link.Arrow>
 
-          <Link.Arrow href="/contact">Contact</Link.Arrow>
-        </div>
-
-        <div className="flex flex-1 items-center justify-end gap-4">
+        <div className="flex items-center gap-4">
           <SignedIn>
-            <Link.Arrow href="/library">Library</Link.Arrow>
+            <Link.Arrow href="/library" theme="outline" size="md">
+              Library
+            </Link.Arrow>
             <ProfileButton />
           </SignedIn>
 
           <SignedOut>
-            <Link.Arrow href="/signin">Sign in</Link.Arrow>
-            <Link.Arrow href="/signup" theme="primary">
+            <Link.Arrow href="/signin" theme="outline" size="md">
+              Sign in
+            </Link.Arrow>
+            <Link.Arrow href="/signup" theme="primary" size="md" className="font-semibold">
               Start Your Adventure
             </Link.Arrow>
           </SignedOut>
@@ -46,37 +45,63 @@ export default function TopNav() {
         <Menu
           placement="bottom-end"
           rootSelector="nav"
+          animation="appear"
+          className="bg-kolumblue-500 font-belanosima outline outline-[0.75px] outline-offset-1 outline-kolumblue-500"
           buttonProps={{
             variant: "unset",
             size: "unset",
             className:
-              "text-scale-md px-4 py-1 rounded-lg bg-kolumblue-500 text-white font-semibold outline-2 outline-double outline-offset-1 outline-kolumblue-500",
+              "text-lg px-5 py-1 rounded-2xl bg-kolumblue-500 text-white font-semibold outline outline-1 hover:rounded focus:rounded ease-kolumb-flow duration-500 outline-offset-1 outline-kolumblue-500 focus:outline-kolumblue-500 focus:outline focus:outline-1",
             children: "Menu",
           }}
         >
           <SignedIn>
-            <MenuLink href="/library" label="library">
+            <MenuLink
+              href="/library"
+              label="library"
+              className="h-10 fill-white pr-8 text-base font-semibold text-white"
+              classNames={{ button: "before:bg-kolumblue-400/50" }}
+            >
               <Icons.library className="ml-2 h-4 w-4" />
               Library
             </MenuLink>
 
-            <Divider className="my-2 bg-gray-100 dark:bg-gray-800" />
+            <Divider className="my-2 bg-kolumblue-400" />
 
-            <MenuLink label="my account" href={accountUrl} className="pr-10">
+            <MenuLink
+              label="my account"
+              href={accountUrl}
+              className="h-10 fill-white pr-8 text-base font-semibold text-white"
+              classNames={{ button: "before:bg-kolumblue-400/50" }}
+            >
               <Icons.userSettings className="ml-2 h-4 w-4" />
               My account
             </MenuLink>
-            <MenuOption label="sign out">
+            <MenuOption
+              label="sign out"
+              className="h-10 fill-white pr-8 text-base font-semibold text-white"
+              classNames={{ button: "before:bg-kolumblue-400/50" }}
+            >
               <Icons.signOut className="ml-2 h-4 w-4" />
               Sign out
             </MenuOption>
           </SignedIn>
 
           <SignedOut>
-            <MenuLink href="/signin" label="sign in">
+            <MenuLink
+              href="/signin"
+              label="sign in"
+              className="h-10 pr-8 text-base font-semibold text-white"
+              classNames={{ button: "before:bg-kolumblue-400/50" }}
+            >
               Sign in
             </MenuLink>
-            <MenuLink href="/signup" label="sign up">
+            <MenuLink
+              href="/signup"
+              label="sign up"
+              className="h-10 pr-8 text-base font-semibold text-white"
+              classNames={{ button: "before:bg-kolumblue-400/50" }}
+            >
               Sign up
             </MenuLink>
           </SignedOut>
