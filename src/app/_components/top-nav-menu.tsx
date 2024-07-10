70d608e600cc8837b2ec03ebf16b359844be2e31
Foz3R <pawel.knot.1@gmail.com>
2024-07-10 21:16:52 +0200

    feat: Add TopNavMenu component for top navigation menu

---
+++ src/app/_components/top-nav-menu.tsx
@@ -0,0 +1,149 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
+import { SignedIn, SignedOut, useClerk } from "@clerk/nextjs";
+
+import { accountUrl } from "~/lib/constants";
+import { EASING } from "~/lib/motion";
+import { cn } from "~/lib/utils";
+
+import SlideAnimation from "./slide-animation";
+import { Menu, MenuLink, MenuOption } from "~/components/ui/menu";
+import { Divider, Icons } from "~/components/ui";
+
+export default function TopNavMenu() {
+  const { signOut } = useClerk();
+  const router = useRouter();
+
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const handleSignOut = async () => {
+    await signOut();
+    router.push("/");
+  };
+
+  return (
+    <div className="absolute right-8 h-16 overflow-hidden py-3.5 pl-0.5">
+      <SlideAnimation direction="in" threshold={100} className="mr-0.5">
+        <Menu
+          isOpen={isMenuOpen}
+          setIsOpen={setMenuOpen}
+          placement="bottom-end"
+          rootSelector="nav"
+          offset={({ rects }) => ({ mainAxis: -rects.reference.height })}
+          className={cn(
+            "whitespace-nowrap rounded bg-kolumblue-500 p-0 font-belanosima outline outline-1 outline-offset-1 outline-kolumblue-500 duration-400 ease-kolumb-flow",
+          )}
+          customAnimation={{
+            initial: { width: "80px", height: "36px", borderRadius: "4px" },
+            animate: {
+              width: "150px",
+              height: "158px",
+              borderRadius: "12px",
+              transition: { ease: EASING.kolumbFlow, duration: 0.6 },
+            },
+            exit: {
+              width: "80px",
+              height: "36px",
+              borderRadius: "16px",
+              transition: { ease: EASING.kolumbFlow, duration: 0.6 },
+            },
+          }}
+          buttonProps={{
+            variant: "unset",
+            size: "unset",
+            animate: { opacity: isMenuOpen ? 0 : 1 },
+            transition: { duration: 0, delay: isMenuOpen ? 0 : 0.6 },
+            className:
+              "text-lg w-20 py-1 rounded-2xl bg-kolumblue-500 text-white outline outline-1 hover:rounded focus:rounded ease-kolumb-flow duration-400 outline-offset-1 outline-kolumblue-500 focus:outline-kolumblue-500 focus:outline focus:outline-1",
+            children: "Menu",
+          }}
+        >
+          <button
+            onClick={() => setMenuOpen(!isMenuOpen)}
+            className="ml-auto h-8 w-20 flex-shrink-0 overflow-hidden py-1 text-lg text-white"
+          >
+            <motion.div
+              animate={{ translateY: -28 }}
+              exit={{ translateY: 0 }}
+              transition={{ delay: 0.1 }}
+            >
+              Menu
+            </motion.div>
+            <motion.div
+              animate={{ translateY: -28 }}
+              exit={{ translateY: 0 }}
+              transition={{ delay: 0.1 }}
+            >
+              Close
+            </motion.div>
+          </button>
+
+          <div className="px-1.5">
+            <SignedIn>
+              <MenuLink
+                href="/library"
+                label="library"
+                className="h-10 fill-white pr-8 text-base text-white"
+                classNames={{ button: "before:bg-kolumblue-400/50" }}
+              >
+                <Icons.library className="ml-2 h-4 w-4" />
+                Library
+              </MenuLink>
+
+              <Divider className="my-2 bg-kolumblue-400" />
+
+              <MenuLink
+                label="my account"
+                href={accountUrl}
+                className="h-10 fill-white pr-8 text-base text-white"
+                classNames={{ button: "before:bg-kolumblue-400/50" }}
+              >
+                <Icons.userSettings className="ml-2 h-4 w-4" />
+                My account
+              </MenuLink>
+              <MenuOption
+                label="sign out"
+                onClick={handleSignOut}
+                className="h-10 fill-white pr-8 text-base text-white"
+                classNames={{ button: "before:bg-kolumblue-400/50" }}
+              >
+                <Icons.signOut className="ml-2 h-4 w-4" />
+                Sign out
+              </MenuOption>
+            </SignedIn>
+
+            <SignedOut>
+              <MenuLink
+                href="/contact"
+                label="sign in"
+                className="h-10 pr-8 text-base text-white"
+                classNames={{ button: "before:bg-kolumblue-400/50" }}
+              >
+                Contact
+              </MenuLink>
+              <MenuLink
+                href="/signin"
+                label="sign in"
+                className="h-10 pr-8 text-base text-white"
+                classNames={{ button: "before:bg-kolumblue-400/50" }}
+              >
+                Sign in
+              </MenuLink>
+              <MenuLink
+                href="/signup"
+                label="sign up"
+                className="h-10 pr-8 text-base text-white"
+                classNames={{ button: "before:bg-kolumblue-400/50" }}
+              >
+                Sign up
+              </MenuLink>
+            </SignedOut>
+          </div>
+        </Menu>
+      </SlideAnimation>
+    </div>
+  );
+}
