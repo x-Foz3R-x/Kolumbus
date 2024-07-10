35529477c78045bf17d66cc23a745c19b0062162
Foz3R <pawel.knot.1@gmail.com>
2024-07-10 21:16:56 +0200

    add top-nav-menu to top-nav component

--- src/app/_components/top-nav.tsx
+++ src/app/_components/top-nav.tsx
@@ -1,28 +1,33 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 
-import { accountUrl } from "~/lib/constants";
-
 import Logo from "./logo";
+import TopNavMenu from "./top-nav-menu";
 import SlideAnimation from "./slide-animation";
 import ProfileButton from "~/components/profile-button";
-import { Menu, MenuLink, MenuOption } from "~/components/ui/menu";
-import { Link, Icons, Divider } from "~/components/ui";
+import { Link } from "~/components/ui";
 
 export default function TopNav() {
   return (
     <nav className="apply-custom-cursor fixed inset-x-0 top-0 z-50 flex h-16 items-center px-8 font-belanosima">
       <Logo />
 
+      {/* Unraveled menu */}
       <SlideAnimation
         direction="out"
         threshold={100}
-        className="relative flex h-16 w-full items-center justify-between gap-6 pl-[224px]"
+        className="relative flex h-16 w-full items-center justify-end gap-6 pl-[224px]"
       >
-        <Link.Arrow href="/contact" theme="outline" size="md">
-          Contact
-        </Link.Arrow>
-
         <div className="flex items-center gap-4">
+          <Link.Arrow href="/contact" size="sm" className="fill-gray-600 text-gray-600">
+            Contact
+          </Link.Arrow>
+          <Link.Arrow href="/contact" size="sm" className="fill-gray-600 text-gray-600">
+            About
+          </Link.Arrow>
+          <Link.Arrow href="/contact" size="sm" className="fill-gray-600 text-gray-600">
+            Pricing
+          </Link.Arrow>
+
           <SignedIn>
             <Link.Arrow href="/library" theme="outline" size="md">
               Library
@@ -41,72 +46,7 @@ export default function TopNav() {
         </div>
       </SlideAnimation>
 
-      <SlideAnimation direction="in" threshold={100} className="absolute right-8">
-        <Menu
-          placement="bottom-end"
-          rootSelector="nav"
-          animation="appear"
-          className="bg-kolumblue-500 font-belanosima outline outline-[0.75px] outline-offset-1 outline-kolumblue-500"
-          buttonProps={{
-            variant: "unset",
-            size: "unset",
-            className:
-              "text-lg px-5 py-1 rounded-2xl bg-kolumblue-500 text-white font-semibold outline outline-1 hover:rounded focus:rounded ease-kolumb-flow duration-500 outline-offset-1 outline-kolumblue-500 focus:outline-kolumblue-500 focus:outline focus:outline-1",
-            children: "Menu",
-          }}
-        >
-          <SignedIn>
-            <MenuLink
-              href="/library"
-              label="library"
-              className="h-10 fill-white pr-8 text-base font-semibold text-white"
-              classNames={{ button: "before:bg-kolumblue-400/50" }}
-            >
-              <Icons.library className="ml-2 h-4 w-4" />
-              Library
-            </MenuLink>
-
-            <Divider className="my-2 bg-kolumblue-400" />
-
-            <MenuLink
-              label="my account"
-              href={accountUrl}
-              className="h-10 fill-white pr-8 text-base font-semibold text-white"
-              classNames={{ button: "before:bg-kolumblue-400/50" }}
-            >
-              <Icons.userSettings className="ml-2 h-4 w-4" />
-              My account
-            </MenuLink>
-            <MenuOption
-              label="sign out"
-              className="h-10 fill-white pr-8 text-base font-semibold text-white"
-              classNames={{ button: "before:bg-kolumblue-400/50" }}
-            >
-              <Icons.signOut className="ml-2 h-4 w-4" />
-              Sign out
-            </MenuOption>
-          </SignedIn>
-
-          <SignedOut>
-            <MenuLink
-              href="/signin"
-              label="sign in"
-              className="h-10 pr-8 text-base font-semibold text-white"
-              classNames={{ button: "before:bg-kolumblue-400/50" }}
-            >
-              Sign in
-            </MenuLink>
-            <MenuLink
-              href="/signup"
-              label="sign up"
-              className="h-10 pr-8 text-base font-semibold text-white"
-              classNames={{ button: "before:bg-kolumblue-400/50" }}
-            >
-              Sign up
-            </MenuLink>
-          </SignedOut>
-        </Menu>
-      </SlideAnimation>
+      <TopNavMenu />
     </nav>
   );
 }
