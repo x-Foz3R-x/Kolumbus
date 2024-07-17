e42630d857439cabe7eb62c150435ac593e55647
Foz3R <pawel.knot.1@gmail.com>
2024-07-17 21:11:14 +0200

    refactor: Update Introduction component layout and content

---
+++ src/app/_components/introduction.tsx
@@ -0,0 +1,67 @@
+export default function Introduction() {
+  return (
+    <section className="bg-white px-8 py-16">
+      <div className="mx-auto max-w-4xl text-left">
+        <h2 className="mb-4 text-4xl font-bold">Discover Kolumbus</h2>
+
+        <div className="flex gap-4">
+          <p className="mb-8 text-lg text-gray-700">
+            Kolumbus is your ultimate travel companion, designed to simplify trip planning and make
+            it enjoyable. Join a community of explorers who trust Kolumbus to craft their perfect
+            itineraries.
+          </p>
+          <p className="mb-8 text-lg text-gray-700">
+            Whether you re a spontaneous adventurer or a meticulous planner, Kolumbus got you
+            covered. Create anything from a day-by-day list of must-see spots to a detailed,
+            hour-by-hour itinerary complete with activities, accommodations, and transportation.
+          </p>
+          <div className="border border-gray-300 p-8">
+            <p className="text-gray-500">
+              [Slider that reveals either spontaneous or precise itinerary illustration]
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    // <section className="bg-white px-8 py-16">
+    //   <div className="mx-auto max-w-4xl text-center">
+    //     <h2 className="mb-4 text-4xl font-bold">Discover Kolumbus</h2>
+    //     <p className="mb-8 text-lg text-gray-700">
+    //       Kolumbus is your ultimate travel companion, designed to simplify trip planning and make it
+    //       enjoyable. Join a community of explorers who trust Kolumbus to craft their perfect
+    //       itineraries.
+    //     </p>
+    //     <p className="mb-8 text-lg text-gray-700">
+    //       Kolumbus is meant to use as a tool to create itineraries as detailed as you want them to
+    //       be. It can be as bear as a day-by-day list of places to visit to make the most of your spontaneous nature or as detailed as a
+    //       hour-by-hour plan with fully featured activities, accommodations, and transportations.
+    //     </p>
+    //     <div className="border border-gray-300 p-8">
+    //       <p className="text-gray-500">[Slider that reveals spontaneous itinerary illustarion or prepared traveler with precie itinerary]</p>
+    //     </div>
+    //   </div>
+    // </section>
+    // <div className="relative font-inter">
+    //   <div className="flex h-fit w-full justify-around rounded-bl-[3rem] bg-gray-800 px-4 pb-16 pt-16 text-white">
+    //     <p className="w-1/3">
+    //       Kolumbus is designed to simplify trip planning and make it fun. Whether you're a seasoned
+    //       traveler or planning your first adventure, Kolumbus provides the tools you need to create
+    //       a perfect itinerary
+    //     </p>
+
+    //     <div className="relative h-[22.5vw] w-[40vw] flex-shrink-0">
+    //       <Image
+    //         src="https://utfs.io/f/680b2d04-1995-4dd2-a959-3222f7d27350-fvhh84.png"
+    //         alt="Itinerary preview"
+    //         fill
+    //       />
+    //     </div>
+    //   </div>
+
+    //   <div className="flex justify-end">
+    //     <span className="relative -z-10 size-12 bg-gray-800 before:absolute before:inset-0 before:rounded-tr-full before:bg-white" />
+    //     <div className="h-28 w-fit rounded-b-[3rem] bg-gray-800 px-4 pt-6"></div>
+    //   </div>
+    // </div>
+  );
+}
