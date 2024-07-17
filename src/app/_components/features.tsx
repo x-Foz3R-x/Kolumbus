41ac82845c2c67a0c1c542141fd63f2398f8267d
Foz3R <pawel.knot.1@gmail.com>
2024-07-17 21:11:23 +0200

    refactor: Update Features component with carousel and arrows

--- src/app/_components/features.tsx
+++ src/app/_components/features.tsx
@@ -3,11 +3,15 @@
 export default function Features() {
   return (
     <div className="relative font-inter">
-      <div className="h-fit w-full rounded-bl-[3rem] bg-gray-800 px-4 py-24 text-white"></div>
+      <div className="h-fit w-full rounded-bl-[3rem] bg-gray-800 px-4 py-24 text-white">
+        Features cards carousel
+      </div>
+
       <div className="flex justify-end">
         <span className="relative -z-10 size-12 bg-gray-800 before:absolute before:inset-0 before:rounded-tr-full before:bg-white" />
 
         <div className="h-28 w-fit rounded-b-[3rem] bg-gray-800 px-4 pt-6 text-white">
+          Arrows
           {"<-- / -->"}
         </div>
       </div>
